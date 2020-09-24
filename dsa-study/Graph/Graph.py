a85f381502c5f6cf069f6027020e1523b12cd9de
Meghal Modi <meghalmodi12@gmail.com>
2020-09-24 13:51:12 -0400

    Added boilerplate code for graph

---
+++ dsa-study/Graph/Graph.py
@@ -0,0 +1,24 @@
+from LinkedList import LinkedList
+
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.arr = []
+
+        for _ in range(vertices):
+            temp = LinkedList()
+            self.arr.append(temp)
+
+    def addEdge(self, source, destination):
+        if source < self.vertices and destination < self.vertices:
+            vertex = self.arr[source]
+            vertex.insertAtHead(destination)
+
+    def printGraph(self):
+        for i in range(len(self.arr)):
+            result = []
+            vertex = i
+            result.append('[' + str(vertex) + ']')
+            result.append(self.arr[i].printList())
+            output = " ".join(result)
+            print(output)
---
+++ dsa-study/Graph/LinkedList.py
@@ -0,0 +1,52 @@
+from Node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def getHead(self):
+        return self.head
+
+    def isEmpty(self):
+        return self.head == None
+
+    def insertAtHead(self, value):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+        else:
+            newNode.next = self.head
+            self.head = newNode
+        return self.head
+
+    def insertAtTail(self, value):
+        newNode = Node(value)
+        if self.tail is None:
+            self.head = newNode
+        else:
+            start = self.head
+            while start.next:
+                start = start.next
+            start.next = newNode
+        return self.head
+
+    def getLength(self):
+        if self.head is None:
+            return 0
+        
+        count = 0
+        start = self.head
+        while start:
+            count += 1
+            start = start.next
+        return count
+
+    def printList(self):
+        elements = []
+        start = self.head
+        while start:
+            elements.append(str(start.value))
+            start = start.next
+        elements.append('None')
+        return "->".join(elements)
+    
